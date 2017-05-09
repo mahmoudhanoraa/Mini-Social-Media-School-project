807e4f9ebbdf09e098588e2fbd542e7d2c49e41a
mazenkh7 <mezokh@gmail.com>
2017-05-09 17:59:32 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/Mini_Social_Media_School_Project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mini_Social_Media_School_Project.dir/main.cpp.o"
+  "Mini_Social_Media_School_Project.pdb"
+  "Mini_Social_Media_School_Project.exe"
+  "libMini_Social_Media_School_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mini_Social_Media_School_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
